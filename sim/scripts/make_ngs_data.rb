8edf37d8c3fbdf4c8f9d2699dca0cdc90477f4f9
lisosia <rainbowseptet@gmail.com>
2016-04-22 22:52:21 +0900

    fix to make correct ngs.csv

--- sim/scripts/make_ngs_data.rb
+++ sim/scripts/make_ngs_data.rb
@@ -1,17 +1,18 @@
 require "csv"
-require_relative "../ngs_csv.rb"
+require_relative "../../ngs_csv.rb"
 
 slides = 1..3
 
 CSV.open("ngs.csv", "w") do |csv|
-  csv << NGS::Headers
+  csv << NGS::HEADERS
   n = 0
   for slide in slides do
     for line_num in 0...(Random.rand(3) + 5) do
       line = []
       line << (line_num == 0 ? slide : nil )
-      for col in NGS::Headers[1..-1] do
-        line << col + "_" + slide.to_s + "_" + Random.rand(100).to_s
+      for col in NGS::HEADERS[1..-1] do
+        line << "#{slide}_runname" and next if col == "run_name"
+        line << col + "_" + slide.to_s + "_" + Random.rand(2).to_s
       end
       csv << line
     end
