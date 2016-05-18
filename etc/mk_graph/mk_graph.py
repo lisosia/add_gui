1b3b7d74b303a1ca0b023daa32c01152ee2ba6d8
lisosia <dev0dsp@gmail.com>
2016-05-18 19:46:05 +0900

    creation of graphs, not integrated with web-page

--- etc/mk_graph/mk_graph.py
+++ etc/mk_graph/mk_graph.py
@@ -14,12 +14,12 @@ indel = []
 
 for i in range( len(ls)/3):
     ii = i*3
-    sample = ls[i].split(',')[0]; ss.append(sample)
+    sample = ls[ii].split(',')[0]; ss.append(sample)
 
     dep.append( float( ls[ii].split(',')[-1] ) )
     dup.append( float( ls[ii+1].split(',')[-1] ))
     snv.append( int( ls[ii+2].split(',')[3] ))
-    indel.append( int(ls[ii+2].split(',')[-1] ) )
+    indel.append( int(ls[ii+2].split(',')[8] ) )
 
 dep = np.array(dep)
 dup = np.array(dup)
@@ -28,7 +28,20 @@ indel = np.array(indel)
 
 X = np.arange( len(ss) )
 
-plt.bar(X, dep) # koreha iede kaku
+plt.subplot(311)
+plt.bar(X, dep, align='center')
+plt.xticks(X, ss, rotation=90, fontsize='small')
 
+plt.subplot(312)
+plt.bar(X, dup, align='center')
+plt.xticks(X, ss, rotation=90, fontsize='small')
+
+plt.subplot(313)
+w = 0.4
+plt.bar(X, snv, width=w ,color='b' ,align='center')
+plt.bar(X + w , indel, width=w ,color='r',align='center')
+plt.xticks(X +w/2, ss, rotation=90, fontsize='small')
+
+plt.tight_layout()
 plt.savefig('tmp.png')
 
