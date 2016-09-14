8fd40f23dba463aa270bbac2f63c3a4c65a257b0
lisosia <rainbowseptet@gmail.com>
2016-09-14 15:04:58 +0900

    graph ylim ytick width

--- etc/mk_graph/mk_graph.py
+++ etc/mk_graph/mk_graph.py
@@ -27,23 +27,31 @@ ssnv = np.array(snv)
 indel = np.array(indel)
 
 X = np.arange( len(ss) )
+margin = 1.
 
-plt.figure( figsize=(7,10) )
+plt.figure( figsize=(10,10) )
+width = 0.5
 
 plt.subplot(311)
 plt.ylabel('depth')
-plt.bar(X, dep, align='center')
+plt.bar(X, dep, align='center', width=width)
 plt.xticks(X, ss, rotation=90, fontsize='small')
-plt.yticks( np.arange(0, max(dep)+30  ,10) , fontsize='small')
+ymax = 150
+plt.yticks( np.arange(0, ymax  ,30) , fontsize='small')
+plt.xlim( -margin , len(ss) -1 + width + margin)
+plt.ylim(0, ymax )
 plt.gca().yaxis.grid(True)
 for x,y in zip(X, dep):
-    plt.text(x,y+3,int(round(y)), ha='center', va='bottom', fontsize='x-small')
+    plt.text(x, min(y ,ymax) +3,int(round(y)), ha='center', va='bottom', fontsize='x-small')
 
 plt.subplot(312)
 plt.ylabel('duplication rate')
-plt.bar(X, dup, align='center')
+plt.bar(X, dup, align='center', width=width)
+plt.xlim( -margin, len(ss) -1 + width + margin)
 plt.xticks(X, ss, rotation=90, fontsize='small')
-plt.yticks( np.arange(0, max(dup) +3 ,1) , fontsize='small')
+ytick_step = int ( max(dup) / 10 )
+ytick_step = max( ytick_step, 1 )
+plt.yticks( np.arange(0, max(dup) + ytick_step * 3  ,ytick_step) , fontsize='small')
 plt.gca().yaxis.grid(True)
 for x,y in zip(X, dup):
     plt.text(x,y+0.5,int(round(y)), ha='center', va='bottom', fontsize='small')
@@ -53,6 +61,7 @@ plt.ylabel('# of rare variants')
 w = 0.4
 plt.bar(X, snv, width=w ,color='b' ,align='center', label='SNV(NS/SS)')
 plt.bar(X + w , indel, width=w ,color='r',align='center', label='indel(coding)')
+plt.xlim( -margin, len(ss) -1 + width + margin)
 plt.legend(loc='upper right', prop={'size':10})
 plt.xticks(X +w/2, ss, rotation=90, fontsize='small')
 plt.yticks( np.arange(0, max(max( snv ),max(indel ) ) + 200  ,100) , fontsize='small')
